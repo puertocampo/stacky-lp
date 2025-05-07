ca72edaa54180dbe1a3342bae49e684cabcbb52c
津野蒼 <puertocampo@gmail.com>
2025-05-07 09:11:05 +0900

    ポーリングでhot reload一応きくようになった

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,16 @@ const nextConfig = {
   trailingSlash: true,
   distDir: "out",
   reactStrictMode: false,
+  webpack: (config, { dev, isServer }) => {
+    // 開発環境でのみ適用
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000, // 1秒ごとにポーリング
+        aggregateTimeout: 300, // 変更を検知してから再ビルドまでの待機時間
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig
