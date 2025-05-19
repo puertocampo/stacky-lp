a8ff32efec7a3a64f3c5d9156e5e74b9e2aaf6e4
津野蒼 <puertocampo@gmail.com>
2025-05-19 09:36:12 +0900

    commentout rights

--- app/page.tsx
+++ app/page.tsx
@@ -352,9 +352,9 @@ export default function LandingPage() {
       </main>
       <footer className="w-full border-t py-6 md:py-0">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+          {/* <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             © {new Date().getFullYear()} Stacky. All rights reserved.
-          </p>
+          </p> */}
           <div className="flex items-center gap-4">
             <Link
               href="#"
